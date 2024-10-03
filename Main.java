ed5f1ae0fcea29233e350ac629c116e7c223b3ca
petyrdimmick <petyr.dimmick@gmail.com>
2024-10-03 20:43:34 +0000

    1

--- Main.java
+++ Main.java
@@ -2,98 +2,70 @@
 // COLLABORATORS: 
 // DATE: 
 
-public class Main
+public class Main 
 {
-    public static void main(String[] args)
-    {
-        //ascii art credit: http://www.oocities.org/spunk1111/bodypart.htm
-        final String ASCII_ART_0 = "    .----.    .----.  ",
-                ASCII_ART_1 = "   (  --  \\  /  --  )",
-                ASCII_ART_2 = "          |  |        ",
-                ASCII_ART_3 = "         _/  \\_      ",
-                ASCII_ART_4 = "        (_    _)      ",
-                ASCII_ART_5 = "     ,    `--`    ,   ",
-                ASCII_ART_6 = "     \\'-.______.-'/  ",
-                ASCII_ART_7 = "      \\          /   ",
-                ASCII_ART_8 = "       '.--..--.'     ",
-                ASCII_ART_9 = "         `\"\"\"\"\"` ",
-                ASCII_CREDIT = "   ascii art by: jgs    ";
-
-        final String TITLE_USA = "UNITED STATES OF AMERICA",
-                TITLE_EAC = "EMPLOYMENT AUTHORIZATION CARD";
-        final String LABEL_SURNAME = "Surname", LABEL_GIVEN_NAME = "Given Name", LABEL_USCIS_NUM = "USCIS#",
-                LABEL_CATEGORY = "Category", LABEL_CARD_NUM = "Card#", LABEL_BIRTH_COUNTRY = "Country of Birth",
-                LABEL_TERMS_CONDITIONS = "Terms and Conditions", LABEL_BIRTH_DATE = "Date of Birth",
-                LABEL_SEX = "Sex", LABEL_VALID_DATE = "Valid From:", LABEL_EXPIRE_DATE = "Card Expires:",
-                LABEL_REENTRY_DISCLAIMER = "NOT VALID FOR REENTRY TO U.S.";
-
-        //DECLARATION SECTION
-        //complete fields
-        String surname, givenName, category, cardNum, birthCountry, termsAndConditions;
-        char sex;
-
-        //parts of fields
-        String birthMonth;
-        int uscisNum1, uscisNum2, uscisNum3, birthDay, birthYear,
-                validDay, validMonth, validYear, expireMonth, expireDay, expireYear;
-
-        //extra vars to help with formatting of Strings (not raw data like above vars)
-        String uscisNum, dateOfBirth, validDate, expireDate;
-
-        //INITIALIZATION SECTION
-        surname = "CHAPETON-LAMAS";
-        givenName = "NERY";
-        uscisNum1 = 12;
-        uscisNum2 = 4;
-        uscisNum3 = 789;
-        category = "C09";
-        cardNum = "SRC9876543210";
-        birthCountry = "Guatemala";
-        termsAndConditions = "None";
-
-        birthDay = 1;
-        birthMonth = "JAN";
-        birthYear = 1970;
-
-        sex = 'M'; //note single quotes
-
-        validMonth = 2;
-        validDay = 2;
-        validYear = 2020;
-
-        expireMonth = 2;
-        expireDay = 2;
-        expireYear = 2022;
-
-        //Strings to help clean up long printf's below
-        uscisNum = String.format("%03d-%03d-%03d", uscisNum1, uscisNum2, uscisNum3);
-        dateOfBirth = String.format("%02d %s %d", birthDay, birthMonth, birthYear);
-        validDate = String.format("%02d/%02d/%4d", validMonth, validDay, validYear);
-        expireDate = String.format("%02d/%02d/%4d", expireMonth, expireDay, expireYear);
-
-
-        //INPUT + CALCULATION SECTION
-        //N/A
-
-        //OUTPUT SECTION
-        System.out.printf("╔══════════════════════════════════════════════════════════════════════╗%n");
-        System.out.printf("║%35s%35s║%n", TITLE_USA, "");
-        System.out.printf("║%60s%10s║%n", TITLE_EAC, "");
-        System.out.printf("║%-25s%-45s║%n", "", LABEL_SURNAME);
-        System.out.printf("║%-25s%-45s║%n", "", surname);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_0, LABEL_GIVEN_NAME);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_1, givenName);
-        System.out.printf("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_2, LABEL_USCIS_NUM, LABEL_CATEGORY, LABEL_CARD_NUM);
-        System.out.printf("║%-25s%-15s%-15s%-15s║%n", ASCII_ART_3, uscisNum, category, cardNum);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_4, LABEL_BIRTH_COUNTRY);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_5, birthCountry);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_6, LABEL_TERMS_CONDITIONS);
-        System.out.printf("║%-25s%-45s║%n", ASCII_ART_7, termsAndConditions);
-        System.out.printf("║%-25s%-15s%-30s║%n", ASCII_ART_8, LABEL_BIRTH_DATE, LABEL_SEX);
-        System.out.printf("║%-25s%-15s%-30s║%n", ASCII_ART_9, dateOfBirth, sex);
-        System.out.printf("║%-25s%-15s%-30s║%n", "", LABEL_VALID_DATE, validDate);
-        System.out.printf("║%-25s%-15s%-30s║%n", "", LABEL_EXPIRE_DATE, expireDate);
-        System.out.printf("║%-25s%-45s║%n", ASCII_CREDIT, LABEL_REENTRY_DISCLAIMER);
-        System.out.printf("╚══════════════════════════════════════════════════════════════════════╝%n");
-    }
-}
+        public static void main(String[] args) 
+        {
+            String surname = "CHAPETON-LAMAS";
+            String givenName = "NERY";
+            String category = "C09";
+            String cardNum = "SRC9876543210";
+            String birthCountry = "Guatemala";
+            String termsAndConditions = "None";
+            char sex = 'M';
+            
+            int uscisNum1 = 12;
+            int uscisNum2 = 4;
+            int uscisNum3 = 789;
+            String uscisNum = String.format("%03d-%03d-%03d", uscisNum1, uscisNum2, uscisNum3);
+            
+            String dateOfBirth = formatDate(1, "JAN", 1970);
+            
+            String validDate = formatDate(2, 2, 2020);
+            
+            String expireDate = formatDate(2, 2, 2022);
+            
+            String formattedCard = formatCard(surname, givenName, category, cardNum, birthCountry,
+                                              termsAndConditions, sex, uscisNum, dateOfBirth, validDate, expireDate);
+            System.out.println(formattedCard);
+        }
+    
+        public static String formatDate(int day, int month, int year) 
+        {
+            return String.format("%02d/%02d/%04d", day, month, year);
+        }
+    
+        public static String formatDate(int day, String month, int year) 
+        {
+            return String.format("%02d %s %04d", day, month, year);
+        }
+    
+        public static String formatCard(String surname, String givenName, String category, String cardNum,
+                                        String birthCountry, String termsAndConditions, char sex, 
+                                        String uscisNum, String dateOfBirth, String validDate, String expireDate) 
+        {
+            StringBuilder card = new StringBuilder();
+            
+            card.append(String.format("╔══════════════════════════════════════════════════════════════════════╗%n"));
+            card.append(String.format("║           UNITED STATES OF AMERICA                                   ║%n"));
+            card.append(String.format("║                               EMPLOYMENT AUTHORIZATION CARD          ║%n"));
+            card.append(String.format("║                         Surname                                      ║%n"));
+            card.append(String.format("║                         %-45s║%n", surname));
+            card.append(String.format("║    .----.    .----.     Given Name                                   ║%n"));
+            card.append(String.format("║   (  --  \\  /  --  )    %-45s║%n", givenName));
+            card.append(String.format("║          |  |           USCIS#         Category       Card#          ║%n"));
+            card.append(String.format("║         _/  \\_          %-15s%-15s%-15s║%n", uscisNum, category, cardNum));
+            card.append(String.format("║        (_    _)         Country of Birth                             ║%n"));
+            card.append(String.format("║     ,    `--`    ,      %-45s║%n", birthCountry));
+            card.append(String.format("║     \\'-.______.-'/      Terms and Conditions                         ║%n"));
+            card.append(String.format("║      \\          /       %-45s║%n", termsAndConditions));
+            card.append(String.format("║       '.--..--.'        Date of Birth  Sex                           ║%n"));
+            card.append(String.format("║         `\"\"\"\"\"`         %-15s%-30s║%n", dateOfBirth, sex));
+            card.append(String.format("║                         Valid From:    %-30s║%n", validDate));
+            card.append(String.format("║                         Card Expires:  %-30s║%n", expireDate));
+            card.append(String.format("║   ascii art by: jgs     NOT VALID FOR REENTRY TO U.S.                ║%n"));
+            card.append(String.format("╚══════════════════════════════════════════════════════════════════════╝"));
+            
+            return card.toString();
+        }
+    }    
